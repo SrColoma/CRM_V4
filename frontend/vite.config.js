28c467c1f08bd0c46027f9eb6b7283ccaa3ff97f
Alfred Coloma <SrColoma.oficial@gmail.com>
2024-01-08 10:33:25 -0500

    Fix server URL in appSettings.json and vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,8 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'http://localhost:8888/';
+      // : 'http://localhost:8888/';
+      : 'http://crm-backend:8888';
 
   const config = {
     plugins: [react()],
